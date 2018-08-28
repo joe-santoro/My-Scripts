2ae918052eb6e410162740946d4b0a912bda3a9e
joe-santoro <32933480+joe-santoro@users.noreply.github.com>
2018-08-28 09:09:14 -0400

    Add files via upload

---
+++ JDK/installjdk.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+date /t >> c:\jdk\installjdk_log.txt
+time /t >> c:\jdk\installjdk_log.txt
+for /f  %%a in (c:\JDK\Listjdk.txt) do (
+	echo "----" >> c:\jdk\installjdk_log.txt
+	echo %%a >> c:\jdk\installjdk_log.txt
+	net use x: \\%%a\c$\temp /persistent:no >> c:\jdk\installjdk_log.txt
+	copy C:\JDK\jdk-8u181-windows*.exe x:\  >> c:\jdk\installjdk_log.txt
+	C:\PStools\PsExec.exe \\%%a cmd /c c:\temp\jdk-8u181-windows-x64.exe /s >> c:\jdk\installjdk_log.txt
+	C:\PStools\PsExec.exe \\%%a cmd /c c:\temp\jdk-8u181-windows-i586.exe /s >> c:\jdk\installjdk_log.txt
+	del x:\jdk-8u181-windows*.exe >> c:\jdk\installjdk_log.txt
+	net use x: /delete >> c:\jdk\installjdk_log.txt
+	echo %%a >> c:\jdk\installjdk_log.txt
+	echo "----" >> c:\jdk\installjdk_log.txt
+)
+date /t >> c:\jdk\installjdk_log.txt
+time /t >> c:\jdk\installjdk_log.txt
