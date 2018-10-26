2f371394a2fa95955241c02212461b2e112c462c
joe-santoro <32933480+joe-santoro@users.noreply.github.com>
2018-10-26 09:14:53 -0400

    Updates

--- JDK/test.bat
+++ JDK/test.bat
@@ -5,10 +5,7 @@ for /f  %%a in (c:\JDK\Listjdk.txt) do (
 	echo "----" >> c:\jdk\installjdk_log.txt
 	echo %%a >> c:\jdk\installjdk_log.txt
 	net use x: \\%%a\c$\temp /persistent:no >> c:\jdk\installjdk_log.txt
-	copy C:\JDK\jdk-8u192-windows*.exe x:\  >> c:\jdk\installjdk_log.txt
-	C:\PStools\PsExec.exe \\%%a cmd /c c:\temp\jdk-8u192-windows-x64.exe /s >> c:\jdk\installjdk_log.txt
-	C:\PStools\PsExec.exe \\%%a cmd /c c:\temp\jdk-8u192-windows-i586.exe /s >> c:\jdk\installjdk_log.txt
-	del x:\jdk-8u192-windows*.exe >> c:\jdk\installjdk_log.txt
+	del x:\jdk-8u191-windows*.exe >> c:\jdk\installjdk_log.txt
 	net use x: /delete >> c:\jdk\installjdk_log.txt
 	echo %%a >> c:\jdk\installjdk_log.txt
 	echo "----" >> c:\jdk\installjdk_log.txt
